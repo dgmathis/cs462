ac6421cf1122a8443166091d6efebbb47f76d47c
David Mathis <flashmathis@gmail.com>
2013-04-05 16:30:59 -0600

    Fixed a bug with viewing teams

--- ufam/api/V1.php
+++ ufam/api/V1.php
@@ -15,4 +15,44 @@ class V1 extends API {
 		print json_encode($activities);
 		die();
 	}
+	
+	public function rfq_create_activity($id) {
+		
+		if(empty($_POST['title'])) {
+			print 'Please provide a title for the activity';
+			die();
+		}
+		
+		if(empty($_POST['date'])) {
+			print 'Please provide a date for the activity';
+			die();
+		}
+		
+		if(empty($_POST['location'])) {
+			print 'Please provide a location for the activity';
+			die();
+		}
+		
+		if(empty($_POST['description'])) {
+			print 'Please provide a description for the activity';
+			die();
+		}
+		
+		$activitysModel = $this->getModel('Activitys');
+		
+		$activity['title'] = $_POST['title'];
+		$activity['date'] = $_POST['date'];
+		$activity['location'] = $_POST['location'];
+		$activity['description'] = $_POST['description'];
+		
+		$result = $activitysModel->addActivity($activity);
+			
+		if($result['code'] == 0) {
+			print 'Failed to create activity';
+		} else {
+			print 'success';
+		}
+		
+		die();
+	}
 }
